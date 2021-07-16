92f0e3065239bcd36aa9e707dee1ea75ca7b885b
anurag <anuragsingh5478@gmail.com>
2021-07-16 14:36:41 +0530

    updated bookingList Component

--- client/src/components/bookingList/bookingList.js
+++ client/src/components/bookingList/bookingList.js
@@ -1,56 +1,9 @@
 import axios from "axios";
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
+import BookingListCard from "./bookingListCard";
 import "./bookingList.css";
 
-function BookingDetailCol(props) {
-  return (
-    <div className="booking-info-card">
-      <div className="booking-info">
-        Booking Date:{" "}
-        <span className="booking-info-value">{props.booking_date}</span>
-      </div>
-      <div className="booking-info">
-        CheckIn Date:{" "}
-        <span className="booking-info-value">{props.checkin_date}</span>
-      </div>
-      <div className="booking-info">
-        CheckOut Date:{" "}
-        <span className="booking-info-value">{props.checkout_date}</span>
-      </div>
-      <div className="booking-info">
-        Number of Rooms:{" "}
-        <span className="booking-info-value">{props.number_of_rooms}</span>
-      </div>
-      <div className="booking-info">
-        Cost:{" "}
-        <span className="booking-info-value">
-          {/* Rupee Symbol */}
-          <span>&#8377;</span>
-          {props.cost}
-        </span>
-      </div>
-      <div className="booking-info-action-buttons">
-        <button className="btn btn-warning" style={{ margin: "0px 5px" }}>
-          <Link to={"/user/booking/cancel/" + props.booking_id}>
-            Full cancellation
-          </Link>
-        </button>
-        <button className="btn btn-warning">
-          <Link to={"/user/booking/edit/" + props.booking_id}>
-            Partial cancellation
-          </Link>
-        </button>
-      </div>
-    </div>
-  );
-}
 export default class BookingList extends Component {
   constructor(props) {
     super(props);
@@ -61,13 +14,12 @@ export default class BookingList extends Component {
       "https://hotel-reservation-system-1.herokuapp.com/booking/userBookingsList";
     axios.get(url, { headers: { token: this.props.token } }).then((res) => {
       this.setState({ bookings: res.data.allBookings });
-      console.log(res.data);
     });
   }
   showBooking() {
     return this.state.bookings.map((booking) => {
       return (
-        <BookingDetailCol
+        <BookingListCard
           booking_id={booking._id}
           booking_date={booking.booking_date.substring(0, 10)}
           checkin_date={booking.checkin_date.substring(0, 10)}
@@ -76,19 +28,21 @@ export default class BookingList extends Component {
           number_of_rooms={booking.number_of_rooms}
           cost={booking.cost}
           key={booking._id}
-        ></BookingDetailCol>
+        ></BookingListCard>
       );
     });
   }
   render() {
     return (
       <div className="booking-list container">
-        <div className="booking-list-heading">My Bookings</div>
-        {this.state.bookings.length == 0 ? (
-          <div className="booking-list-empty-text">Booking List Is Empty</div>
-        ) : (
-          this.showBooking()
-        )}
+        <div className="main-title">My Bookings</div>
+        <div className="container">
+          {this.state.bookings.length == 0 ? (
+            <div className="booking-list-empty-text">Booking List Is Empty</div>
+          ) : (
+            this.showBooking()
+          )}
+        </div>
       </div>
     );
   }
---
+++ client/src/components/bookingList/bookingListCard.js
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import "./bookingList.css";
+
+export default function bookingListCard(props) {
+  return (
+    <div className="booking-info-card row">
+      <div className="col-md-8">
+        <div className="booking-info">
+          Booking Date:{" "}
+          <span className="booking-info-value">{props.booking_date}</span>
+        </div>
+        <div className="booking-info">
+          CheckIn Date:{" "}
+          <span className="booking-info-value">{props.checkin_date}</span>
+        </div>
+        <div className="booking-info">
+          CheckOut Date:{" "}
+          <span className="booking-info-value">{props.checkout_date}</span>
+        </div>
+        <div className="booking-info">
+          Number of Rooms:{" "}
+          <span className="booking-info-value">{props.number_of_rooms}</span>
+        </div>
+        <div className="booking-info">
+          Cost:{" "}
+          <span className="booking-info-value">
+            {/* Rupee Symbol */}
+            <span>&#8377;</span>
+            {Math.round(props.cost)}
+          </span>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="booking-info-action-buttons">
+          <button className="btn btn-warning w-100">
+            <Link to={"/user/booking/cancel/" + props.booking_id}>
+              Full cancellation
+            </Link>
+          </button>
+          <br />
+          <button className="btn btn-warning w-100">
+            <Link to={"/user/booking/edit/" + props.booking_id}>
+              Partial cancellation
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
