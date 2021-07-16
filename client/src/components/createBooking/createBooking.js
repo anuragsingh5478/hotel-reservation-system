2216146421fc6c6ce331926f9dc1967bdff5ef68
anurag <anuragsingh5478@gmail.com>
2021-07-16 14:44:25 +0530

    updated createBooking Component

--- client/src/components/createBooking/createBooking.js
+++ client/src/components/createBooking/createBooking.js
@@ -5,7 +5,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./style.css";
 import { Redirect } from "react-router";
 
-export default class CreateExercise extends Component {
+export default class CreateBooking extends Component {
   constructor(props) {
     super(props);
 
@@ -13,12 +13,14 @@ export default class CreateExercise extends Component {
     this.onChangeCheckoutDate = this.onChangeCheckoutDate.bind(this);
     this.onChangeRoom = this.onChangeRoom.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.dataValidator = this.dataValidator.bind(this);
 
     this.state = {
       checkin_date: new Date(),
       checkout_date: new Date(),
-      number_of_rooms: 0,
+      number_of_rooms: 1,
       msg: "",
+      validatorMsg: "",
     };
   }
 
@@ -40,8 +42,39 @@ export default class CreateExercise extends Component {
     });
   }
 
+  dataValidator() {
+    if (this.state.checkin_date < Date.now()) {
+      let minimumCheckInDate = new Date();
+      minimumCheckInDate.setDate(minimumCheckInDate.getDate() + 1);
+      this.setState({
+        msg:
+          "CheckIn Starts From " +
+          minimumCheckInDate.toString().substr(3, 12) +
+          "(i.e. tomorrow)",
+      });
+      return false;
+    }
+    if (this.state.checkout_date < this.state.checkin_date) {
+      this.setState({
+        msg: "CheckOut Date Can't Be In Earlier than CheckIn Date",
+      });
+      return false;
+    }
+    if (this.state.number_of_rooms < 1) {
+      this.setState({
+        msg: "Number Of Rooms Can't Be Less Than 1",
+      });
+      return false;
+    }
+    return true;
+  }
+
   onSubmit(e) {
     e.preventDefault();
+
+    if (!this.dataValidator()) {
+      return;
+    }
 
     const newBooking = {
       booking_date: new Date(),
@@ -50,7 +83,6 @@ export default class CreateExercise extends Component {
       number_of_rooms: this.state.number_of_rooms,
     };
 
-    console.log(newBooking);
     axios
       .post(
         "https://hotel-reservation-system-1.herokuapp.com/booking/create",
@@ -60,14 +92,12 @@ export default class CreateExercise extends Component {
         }
       )
       .then((res) => this.setState({ msg: res.data.msg }));
-
-    // window.location = "/home/bookinglist/" + this.props.match.params.id;
   }
 
   render() {
     return (
       <div className="booking-creation container">
-        <div className="booking-creation-heading">Create New Booking</div>
+        <div className="main-title">Create New Booking</div>
         <div className="booking-creation-card">
           <form onSubmit={this.onSubmit} className="">
             <div className="form-group">
@@ -81,9 +111,6 @@ export default class CreateExercise extends Component {
             </div>
             <div className="form-group">
               <label>Checkout-Date: </label>
-              <div className="info-text">
-                (Should be greater than CheckIn Date)
-              </div>
               <div>
                 <DatePicker
                   selected={this.state.checkout_date}
@@ -93,7 +120,6 @@ export default class CreateExercise extends Component {
             </div>
             <div className="form-group">
               <label>Number of Rooms: </label>
-              <div className="info-text">(Should be greater than Zero)</div>
               <input
                 type="text"
                 required
@@ -102,7 +128,7 @@ export default class CreateExercise extends Component {
                 onChange={this.onChangeRoom}
               />
             </div>
-
+            <div className="info-text">{this.state.msg}</div>
             <div className="form-group">
               <input
                 type="submit"
